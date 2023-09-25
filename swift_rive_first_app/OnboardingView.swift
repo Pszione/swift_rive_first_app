ed2202ebaf4b66dfbbd5fac7ab2ee7c27f74e3f3
Pedro Santos <pedro.santos@vizo.dev>
2023-09-25 11:56:46 -0300

    adding StartCourse button to Onboarding view

--- swift_rive_first_app/OnboardingView.swift
+++ swift_rive_first_app/OnboardingView.swift
@@ -9,7 +9,34 @@ import SwiftUI
 import RiveRuntime
 
 struct OnboardingView: View {
+    let button = RiveViewModel(fileName: "button")
+    
     var body: some View {
+        ZStack {
+            background
+            
+            button.view()
+                .frame(width: 236, height: 64)
+                .overlay(
+                    Label("Start the course", systemImage: "arrow.forward")
+                    .offset(x: 4, y:4)
+                    .font(.headline)
+                )
+                .background(
+                    Color.black
+                        .cornerRadius(30)
+                        .blur(radius: 10)
+                        .opacity(0.3)
+                        .offset(y: 10)
+                )
+                .onTapGesture {
+                    button.play(animationName: "active")
+                }
+            
+        }
+    }
+    
+    var background: some View {
         RiveViewModel(fileName: "shapes").view()
             .ignoresSafeArea()
             .blur(radius: 30)
@@ -18,7 +45,7 @@ struct OnboardingView: View {
                     .blur(radius: 50)
                     .offset(x: 200, y: 100)
             
-            )
+        )
     }
 }
 

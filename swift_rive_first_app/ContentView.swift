39780ac218116ff1d39270c07c5eca5a422c6153
Pedro Santos <pedro.santos@vizo.dev>
2023-09-25 10:55:07 -0300

    Initial Commit with SwiftUi project

---
+++ swift_rive_first_app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swift_rive_first_app
+//
+//  Created by Pedro Santos on 25/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift_rive_first_app/swift_rive_first_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_rive_first_appApp.swift
+//  swift_rive_first_app
+//
+//  Created by Pedro Santos on 25/09/23.
+//
+
+import SwiftUI
+
+@main
+struct swift_rive_first_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

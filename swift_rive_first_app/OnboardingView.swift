4ec03cc33013a7252e075f47abcec7635c8189f7
Pedro Santos <pedro.santos@vizo.dev>
2023-09-25 11:16:39 -0300

    adding OnboardingView background

---
+++ swift_rive_first_app/OnboardingView.swift
@@ -0,0 +1,27 @@
+//
+//  OnboardingView.swift
+//  swift_rive_first_app
+//
+//  Created by Pedro Santos on 25/09/23.
+//
+
+import SwiftUI
+import RiveRuntime
+
+struct OnboardingView: View {
+    var body: some View {
+        RiveViewModel(fileName: "shapes").view()
+            .ignoresSafeArea()
+            .blur(radius: 30)
+            .background(
+                Image("Spline")
+                    .blur(radius: 50)
+                    .offset(x: 200, y: 100)
+            
+            )
+    }
+}
+
+#Preview {
+    OnboardingView()
+}

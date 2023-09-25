4a5b9602468eb2a7fa9c23bb5a47917e37904641
Pedro Santos <pedro.santos@vizo.dev>
2023-09-25 12:37:09 -0300

    wip - adding CustomFont ViewModifier + extension

--- swift_rive_first_app/CustomFont.swift
+++ swift_rive_first_app/CustomFont.swift
@@ -10,9 +10,36 @@ import SwiftUI
 struct CustomFont: ViewModifier {
     var textStyle: TextStyle
     
+    var name: String {
+        switch textStyle {
+        case .title:
+            return "Poppins Bold"
+        case .body:
+            return "Inter Regular"
+        }
+    }
+    
+    var size: CGFloat {
+        switch textStyle {
+        case .title:
+            return 28
+        case .body:
+            return 17
+        }
+    }
+    
+    var relative: Font.TextStyle{
+        switch textStyle {
+        case .title:
+            return .title
+        case .body:
+            return .body
+        }
+    }
+    
     func body(content: Content) -> some View {
         // note: option + click to see class constructor variables
-        content.font(.custom("Inter Regular", size: 17, relativeTo: .body))
+        content.font(.custom(name, size: size, relativeTo: relative))
     }
 }
 
